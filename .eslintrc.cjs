f292d231b15a8e1302ebbc9a6110ccad2e19ea8e
Antoine <birkhoferantoine@gmail.com>
2023-11-10 18:18:09 +0100

    Firebase & resend emailing is done. Did a bit of work on orb pointer. Added eslint and prettier. v2.1.1 ready for deploy

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "prettier"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "only-warn"
+    ],
+    "rules": {
+        "no-duplicate-imports": "error",
+        "react/prop-types": 0
+    },
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    }
+}
